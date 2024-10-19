127ba82a5746df4bd252a8df95af1f821c0ea87b
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-19 12:08:06 -0500

    changes to fix connection issue

--- db/connect.js
+++ db/connect.js
@@ -1,68 +1,33 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
-// const MongoClient = require('mongodb').MongoClient;
-
-// let _db;
-
-// const initDb = (callback) => {
-//     if (_db) {
-//         console.log('Db is already initialized!');
-//         return callback(null, _db);
-//     }
-
-//     MongoClient.connect(process.env.MONGODB_URI)
-//         .then((client) => {
-//             _db = client;
-//             callback(null, _db);
-//         })
-//         .catch((err) => {
-//             callback(err);
-//         });
-// };
-
-// const getDb = () => {
-//     if (!_db) {
-//         throw Error('Db not initialized');
-//     }
-//     return _db;
-// };
-
-// module.exports = {
-//     initDb,
-//     getDb
-// };
-
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 let _db;
 
-const initDb = callback => {
+const initDb = (callback) => {
     if (_db) {
-        console.log('Database is already initialized!');
+        console.log('Db is already initialized!');
         return callback(null, _db);
     }
 
-    client.connect()
-        .then(client => {
-            _db = client.db();
-            console.log('Database connected!');
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            _db = client;
             callback(null, _db);
         })
-        .catch(err => {
+        .catch((err) => {
             callback(err);
         });
 };
 
 const getDb = () => {
     if (!_db) {
-        throw Error('Database not initialized');
+        throw Error('Db not initialized');
     }
     return _db;
 };
 
-module.exports = { initDb, getDb };
+module.exports = {
+    initDb,
+    getDb
+};
