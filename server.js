28fa7c98351b4dabbf0b610d42618bf35760c724
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-18 23:29:28 -0500

    astronomy observation project start

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { initDb } = require('./db/connect');
+const contactsRouter = require('./routes/contacts');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const app = express();
+const port = 3000;
+
+app
+    .use(bodyParser.json())
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', require('./routes'));
+
+initDb((err) => {
+    if (err) {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1);
+    }
+
+    console.log('MongoDB connected successfully.');
+
+    app.use('/contacts', contactsRouter);
+
+    app.listen(process.env.PORT || port, () => {
+        console.log('Web Server is listening at port ' + (process.env.PORT || port));
+    });
+});
