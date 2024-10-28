bd147eaee011d98b3fff3f5c3b2188175d528559
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-28 02:39:38 -0500

    attempt 7

--- controllers/observation.js
+++ controllers/observation.js
@@ -1,157 +1,9 @@
-// const Observation = require('../models/observation');
-
-// const getObservations = async (req, res) => {
-//     try {
-//         const observations = await Observation.find();
-//         res.setHeader('Content-Type', 'application/json');
-//         res.status(200).json(observations);
-//     } catch (err) {
-//         console.error('Error fetching observations:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const getSingleObservation = async (req, res) => {
-//     const username = req.params.username;
-
-//     if (!username) {
-//         return res.status(400).json({ error: 'Username is required' });
-//     }
-
-//     try {
-//         const observation = await Observation.findOne({ username });
-//         if (!observation) {
-//             return res.status(404).json('Observation not found');
-//         }
-//         res.setHeader('Content-Type', 'application/json');
-//         res.status(200).json(observation);
-//     } catch (err) {
-//         console.error('Error fetching observation:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const createObservation = async (req, res) => {
-//     const newObservation = new Observation({
-//         username: req.body.username,
-//         observation: req.body.observation,
-//         object_type: req.body.object_type,
-//         date: req.body.date,
-//         time: req.body.time,
-//         location: {
-//             country: req.body.location.country,
-//             state: req.body.location.state,
-//             city: req.body.location.city,
-//             coordinates: {
-//                 latitude: req.body.location.coordinates.latitude,
-//                 longitude: req.body.location.coordinates.longitude
-//             }
-//         },
-//         equipment: {
-//             equipment_type: req.body.equipment.equipment_type,
-//             make: req.body.equipment.make,
-//             model: req.body.equipment.make,
-//             serial_number: req.body.equipment.serial_number
-//         },
-//         weather_conditions: req.body.weather_conditions,
-//         visibility_conditions: req.body.visibility_conditions,
-//         duration: req.body.duration,
-//         notes: req.body.notes,
-//     });
-//     try {
-//         await newObservation.save();
-//         res.status(201).json(newObservation);
-//     } catch (err) {
-//         console.error('Error creating observation:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const updateObservation = async (req, res) => {
-//     const username = req.params.username;
-
-//     if (!username) {
-//         return res.status(400).json('Invalid Username Supplied');
-//     }
-
-//     try {
-//         const updatedObservation = await Observation.findOneAndUpdate(
-//             { username },
-//             {
-//                 $set: {
-//                     observation: req.body.observation,
-//                     object_type: req.body.object_type,
-//                     date: req.body.date,
-//                     time: req.body.time,
-//                     location: {
-//                         country: req.body.location.country,
-//                         state: req.body.location.state,
-//                         city: req.body.location.city,
-//                         coordinates: {
-//                             latitude: req.body.location.coordinates.latitude,
-//                             longitude: req.body.location.coordinates.longitude
-//                         }
-//                     },
-//                     equipment: {
-//                         equipment_type: req.body.equipment.equipment_type,
-//                         make: req.body.equipment.make,
-//                         model: req.body.equipment.make,
-//                         serial_number: req.body.equipment.serial_number
-//                     },
-//                     weather_conditions: req.body.weather_conditions,
-//                     visibility_conditions: req.body.visibility_conditions,
-//                     duration: req.body.duration,
-//                     notes: req.body.notes
-//                 }
-//             },
-//             { new: true, runValidators: true }
-//         );
-
-//         if (!updatedObservation) {
-//             return res.status(404).json('Observation not found');
-//         }
-//         res.status(200).json(updatedObservation);
-//     } catch (err) {
-//         console.error('Error updating observation:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const deleteObservation = async (req, res) => {
-//     const username = req.params.username;
-
-//     if (!username) {
-//         return res.status(400).json('Username is required');
-//     }
-
-//     try {
-//         const result = await Observation.deleteOne({ username });
-
-//         if (result.deletedCount === 0) {
-//             return res.status(404).json('Observation not found');
-//         }
-//         res.status(204).send();
-//     } catch (err) {
-//         console.error('Error deleting observation:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// module.exports = {
-//     getObservations,
-//     getSingleObservation,
-//     createObservation,
-//     updateObservation,
-//     deleteObservation
-// };
-
 const mongoose = require('../db/connect');
 const Observation = require('../models/observation')(mongoose);
 
 const getObservations = async (req, res) => {
-    const { username } = req.query;
     try {
-        const observations = await Observation.find({ username });
+        const observations = await Observation.find();
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(observations);
     } catch (err) {
@@ -161,11 +13,16 @@ const getObservations = async (req, res) => {
 };
 
 const getSingleObservation = async (req, res) => {
-    const { username } = req.params;
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).json({ error: 'Username is required' });
+    }
+
     try {
         const observation = await Observation.findOne({ username });
         if (!observation) {
-            return res.status(404).send('Observation not found for this username');
+            return res.status(404).json('Observation not found');
         }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(observation);
@@ -182,7 +39,6 @@ const createObservation = async (req, res) => {
         object_type: req.body.object_type,
         date: req.body.date,
         time: req.body.time,
-        time_zone: req.body.time_zone,
         location: {
             country: req.body.location.country,
             state: req.body.location.state,
@@ -195,18 +51,17 @@ const createObservation = async (req, res) => {
         equipment: {
             equipment_type: req.body.equipment.equipment_type,
             make: req.body.equipment.make,
-            model: req.body.equipment.model,
+            model: req.body.equipment.make,
             serial_number: req.body.equipment.serial_number
         },
         weather_conditions: req.body.weather_conditions,
         visibility_conditions: req.body.visibility_conditions,
         duration: req.body.duration,
-        notes: req.body.notes
+        notes: req.body.notes,
     });
-
     try {
-        const result = await newObservation.save();
-        res.status(201).json(result);
+        await newObservation.save();
+        res.status(201).json(newObservation);
     } catch (err) {
         console.error('Error creating observation:', err);
         res.status(500).send('Internal Server Error');
@@ -214,13 +69,49 @@ const createObservation = async (req, res) => {
 };
 
 const updateObservation = async (req, res) => {
-    const { username } = req.params;
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).json('Invalid Username Supplied');
+    }
+
     try {
-        const result = await Observation.findOneAndUpdate({ username }, req.body, { new: true });
-        if (!result) {
-            return res.status(404).send('Observation not found for this username');
+        const updatedObservation = await Observation.findOneAndUpdate(
+            { username },
+            {
+                $set: {
+                    observation: req.body.observation,
+                    object_type: req.body.object_type,
+                    date: req.body.date,
+                    time: req.body.time,
+                    location: {
+                        country: req.body.location.country,
+                        state: req.body.location.state,
+                        city: req.body.location.city,
+                        coordinates: {
+                            latitude: req.body.location.coordinates.latitude,
+                            longitude: req.body.location.coordinates.longitude
+                        }
+                    },
+                    equipment: {
+                        equipment_type: req.body.equipment.equipment_type,
+                        make: req.body.equipment.make,
+                        model: req.body.equipment.make,
+                        serial_number: req.body.equipment.serial_number
+                    },
+                    weather_conditions: req.body.weather_conditions,
+                    visibility_conditions: req.body.visibility_conditions,
+                    duration: req.body.duration,
+                    notes: req.body.notes
+                }
+            },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedObservation) {
+            return res.status(404).json('Observation not found');
         }
-        res.status(200).json(result);
+        res.status(200).json(updatedObservation);
     } catch (err) {
         console.error('Error updating observation:', err);
         res.status(500).send('Internal Server Error');
@@ -228,11 +119,17 @@ const updateObservation = async (req, res) => {
 };
 
 const deleteObservation = async (req, res) => {
-    const { username } = req.params;
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).json('Username is required');
+    }
+
     try {
-        const result = await Observation.findOneAndDelete({ username });
-        if (!result) {
-            return res.status(404).send('Observation not found for this username');
+        const result = await Observation.deleteOne({ username });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json('Observation not found');
         }
         res.status(204).send();
     } catch (err) {
--- controllers/user.js
+++ controllers/user.js
@@ -1,126 +1,3 @@
-// const User = require('../models/user');
-
-// const getUsers = async (req, res) => {
-//     try {
-//         const users = await User.find();
-//         res.setHeader('Content-Type', 'application/json');
-//         res.status(200).json(users);
-//     } catch (err) {
-//         console.error('Error fetching users:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const getSingle = async (req, res) => {
-//     const username = req.params.username;
-
-//     if (!username) {
-//         return res.status(400).json('Username is required');
-//     }
-
-//     try {
-//         const user = await User.findOne({ username });
-//         if (!user) {
-//             return res.status(404).json('User not found');
-//         }
-//         res.setHeader('Content-Type', 'application/json');
-//         res.status(200).json(user);
-//     } catch (err) {
-//         console.error('Error fetching user:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const createUser = async (req, res) => {
-//     const newUser = new User({
-//         username: req.body.username,
-//         contact_information: {
-//             email: req.body.contact_information.email,
-//             phone_number: req.body.contact_information.phone_number,
-//             preferred_contact: req.body.contact_information.preferred_contact
-//         },
-//         observation_log: {
-//             observations: req.body.observation_log.observations || []
-//         }
-//     });
-
-//     try {
-//         await newUser.save();
-//         res.status(201).json(newUser);
-//     } catch (err) {
-//         console.error('Error creating user:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const updateUser = async (req, res) => {
-//     const username = req.params.username;
-
-//     if (!username) {
-//         return res.status(400).json('Invalid Username Supplied');
-//     }
-
-//     try {
-//         const updatedUser = await User.findOneAndUpdate(
-//             { username },
-//             {
-//                 $set: {
-//                     username: req.body.username,
-//                     contact_information: {
-//                         email: req.body.contact_information.email,
-//                         phone_number: req.body.contact_information.phone_number,
-//                         preferred_contact: req.body.contact_information.preferred_contact
-//                     },
-//                     observation_log: {
-//                         observations: req.body.observation_log.observations || []
-//                     }
-//                 }
-//             },
-//             { new: true, runValidators: true }
-//         );
-
-//         if (!updatedUser) {
-//             return res.status(404).json('User not found');
-//         }
-
-//         res.status(200).json(updatedUser);
-//     } catch (err) {
-//         console.error('Error updating user:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// const deleteUser = async (req, res) => {
-//     const username = req.params.username;
-
-//     if (!username) {
-//         return res.status(400).json('Username is required');
-//     }
-
-//     try {
-//         const result = await User.deleteOne({ username });
-
-//         if (result.deletedCount === 0) {
-//             return res.status(404).json('User not found');
-//         }
-//         res.status(204).send();
-//     } catch (err) {
-//         console.error('Error deleting user:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-
-// module.exports = {
-//     getUsers,
-//     getSingle,
-//     createUser,
-//     updateUser,
-//     deleteUser
-// };
-
-
-// ORIGINAL CODE
-
 const mongoose = require('../db/connect');
 const User = require('../models/user')(mongoose);
 
@@ -136,11 +13,16 @@ const getUsers = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const { username } = req.params;
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).json('Username is required');
+    }
+
     try {
         const user = await User.findOne({ username });
         if (!user) {
-            return res.status(404).send('User not found');
+            return res.status(404).json('User not found');
         }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(user);
@@ -164,8 +46,8 @@ const createUser = async (req, res) => {
     });
 
     try {
-        const result = await newUser.save();
-        res.status(201).json(result);
+        await newUser.save();
+        res.status(201).json(newUser);
     } catch (err) {
         console.error('Error creating user:', err);
         res.status(500).send('Internal Server Error');
@@ -173,13 +55,36 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-    const { username } = req.params;
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).json('Invalid Username Supplied');
+    }
+
     try {
-        const result = await User.findOneAndUpdate({ username }, req.body, { new: true });
-        if (!result) {
-            return res.status(404).send('User not found');
+        const updatedUser = await User.findOneAndUpdate(
+            { username },
+            {
+                $set: {
+                    username: req.body.username,
+                    contact_information: {
+                        email: req.body.contact_information.email,
+                        phone_number: req.body.contact_information.phone_number,
+                        preferred_contact: req.body.contact_information.preferred_contact
+                    },
+                    observation_log: {
+                        observations: req.body.observation_log.observations || []
+                    }
+                }
+            },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json('User not found');
         }
-        res.status(200).json(result);
+
+        res.status(200).json(updatedUser);
     } catch (err) {
         console.error('Error updating user:', err);
         res.status(500).send('Internal Server Error');
@@ -187,11 +92,17 @@ const updateUser = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
-    const { username } = req.params;
+    const username = req.params.username;
+
+    if (!username) {
+        return res.status(400).json('Username is required');
+    }
+
     try {
-        const result = await User.findOneAndDelete({ username });
-        if (!result) {
-            return res.status(404).send('User not found');
+        const result = await User.deleteOne({ username });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json('User not found');
         }
         res.status(204).send();
     } catch (err) {
