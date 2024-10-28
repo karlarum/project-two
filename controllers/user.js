4c06b4bea9441a36b4beeee73971c9cbc2636d84
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-28 01:51:27 -0500

    attempt 3.5

--- controllers/user.js
+++ controllers/user.js
@@ -121,12 +121,12 @@
 
 // ORIGINAL CODE
 
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongoose = require('../db/connect');
+const User = require('../models/user')(mongoose);
+
 const getUsers = async (req, res) => {
     try {
-        const db = mongodb.getDb().db();
-        const users = await db.collection('user').find().toArray();
+        const users = await User.find();
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
     } catch (err) {
@@ -134,26 +134,24 @@ const getUsers = async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 };
+
 const getSingle = async (req, res) => {
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) {
-        return res.status(400).send('Invalid ID format');
-    }
+    const { username } = req.params;
     try {
-        const db = mongodb.getDb().db();
-        const user = await db.collection('user').findOne({ _id: new ObjectId(id) });
+        const user = await User.findOne({ username });
         if (!user) {
             return res.status(404).send('User not found');
         }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(user);
     } catch (err) {
-        console.error('Error fetching contact:', err);
+        console.error('Error fetching user:', err);
         res.status(500).send('Internal Server Error');
     }
 };
+
 const createUser = async (req, res) => {
-    const newUser = {
+    const newUser = new User({
         username: req.body.username,
         contact_information: {
             email: req.body.contact_information.email,
@@ -163,54 +161,36 @@ const createUser = async (req, res) => {
         observation_log: {
             observations: req.body.observation_log.observations || []
         }
-    };
+    });
+
     try {
-        const db = mongodb.getDb().db();
-        const result = await db.collection('user').insertOne(newUser);
-        const createdUser = await db.collection('user').findOne({ _id: result.insertedId });
-        res.status(201).json(createdUser);
+        const result = await newUser.save();
+        res.status(201).json(result);
     } catch (err) {
         console.error('Error creating user:', err);
         res.status(500).send('Internal Server Error');
     }
 };
+
 const updateUser = async (req, res) => {
-    const id = req.params.id;
-    const updatedUser = {
-        username: req.body.username,
-        contact_information: {
-            email: req.body.contact_information.email,
-            phone_number: req.body.contact_information.phone_number,
-            preferred_contact: req.body.contact_information.preferred_contact
-        },
-        observation_log: {
-            observations: req.body.observation_log.observations || []
-        }
-    };
-    if (!ObjectId.isValid(id)) {
-        return res.status(400).send('Invalid ID format');
-    }
+    const { username } = req.params;
     try {
-        const db = mongodb.getDb().db();
-        const result = await db.collection('user').replaceOne({ _id: new ObjectId(id) }, updatedUser);
-        if (result.matchedCount === 0) {
+        const result = await User.findOneAndUpdate({ username }, req.body, { new: true });
+        if (!result) {
             return res.status(404).send('User not found');
         }
-        res.status(204).send();
+        res.status(200).json(result);
     } catch (err) {
         console.error('Error updating user:', err);
         res.status(500).send('Internal Server Error');
     }
 };
+
 const deleteUser = async (req, res) => {
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) {
-        return res.status(400).send('Invalid ID format');
-    }
+    const { username } = req.params;
     try {
-        const db = mongodb.getDb().db();
-        const result = await db.collection('user').deleteOne({ _id: new ObjectId(id) });
-        if (result.deletedCount === 0) {
+        const result = await User.findOneAndDelete({ username });
+        if (!result) {
             return res.status(404).send('User not found');
         }
         res.status(204).send();
@@ -219,6 +199,7 @@ const deleteUser = async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 };
+
 module.exports = {
     getUsers,
     getSingle,
