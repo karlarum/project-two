247fa42b38105dc3c153e8e623fb8b2dff2c5d05
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-28 01:53:31 -0500

    attempt 4

--- db/connect.js
+++ db/connect.js
@@ -7,31 +7,24 @@
 
 // ORIGINAL CODE
 
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
-let _db;
-const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
-  }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-};
-const getDb = () => {
-  if (!_db) {
-    throw Error('Db not initialized');
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
   }
-  return _db;
 };
+
 module.exports = {
-  initDb,
-  getDb
+  connectDB,
+  mongoose,
 };
